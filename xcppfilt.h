a22a6dbcab8e5c45bcde402de992135014ef2b82
horsicq <horsicq@gmail.com>
2022-02-04 17:16:16 +0100

    Fix: 2022-02-04

--- xcppfilt.h
+++ xcppfilt.h
@@ -31,7 +31,6 @@ class XCppfilt : public QObject
 
 public:
     explicit XCppfilt(QObject *pParent=nullptr);
-    // TODO more types
     static QString demangleGnuV3(QString sString);
     static QString demangleJavaV3(QString sString);
     static QString demangleRust(QString sString);
